1bc7dbcc538e938b1853fad9486763bdfdb994a6
OmarPC <o.ibrahim@nuaragroup.com>
2019-06-04 15:07:51 +0800

    add include task, clean task and materializecss

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ const replace = require('gulp-replace');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+const lbInclude = require('gulp-lb-include');
+const rimraf = require('rimraf');
 
 // File path variables
 const files = {
@@ -33,10 +35,17 @@ function jsTask() {
         .pipe(gulp.dest('dist/resource/js'));
 }
 
+// lbInclude task
+function includeTask() {
+    return gulp.src('src/html/**/*.html')
+        .pipe(lbInclude())
+        .pipe(gulp.dest('dist/'));
+}
+
 // Cachebusting task
 const cbString = new Date().getTime();
 function cacheBustTask(){
-    return gulp.src(['src/html/index.html'])
+    return gulp.src(['dist/index.html'])
         .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
         .pipe(gulp.dest('./dist/'));
 }
@@ -47,15 +56,27 @@ function watchTask() {
         gulp.parallel(scssTask, jsTask));
 }
 
+// Custom task
+function cleanTask() {
+    rimraf("dist/component", function(){
+        console.log("folder deleted successfully")
+    })
+    return Promise.resolve();
+}
+
 // Default task
 exports.default = gulp.series(
     gulp.parallel(scssTask, jsTask),
-    cacheBustTask
+    includeTask,
+    cacheBustTask,
+    cleanTask
 );
 
 // Build with watch mode
 exports.watch = gulp.series(
     gulp.parallel(scssTask, jsTask),
+    includeTask,
     cacheBustTask,
+    cleanTask,
     watchTask
 );
